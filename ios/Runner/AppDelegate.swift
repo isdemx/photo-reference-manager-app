3b34139033aeceac26351ceefed8efdd4108bfce
sk <skudryashov@perchapps.com>
2024-10-07 23:52:12 +0300

    fix

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,18 @@
+
 import UIKit
 import Flutter
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+  
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    // Устанавливаем черный фон для всего окна
+    window?.backgroundColor = UIColor.black
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
